7f6d2886eccd56eead36b7e79e7964f2e50e16d6
Reggie <ryoboua@gmail.com>
2018-10-10 15:46:39 -0400

    Update website_raw/src/home_page/Nav.js

--- website_raw/src/home_page/Nav.js
+++ website_raw/src/home_page/Nav.js
@@ -4,32 +4,31 @@ import { Link } from 'react-router-dom';
 
 import './nav.css'
 
+const navOptions = [
+    { title: 'Products', url: '/products' },
+    { title: 'Info', url: '/info' },
+    { title: 'Contact', url: '/contact' },
+]
+const navStyle = (direction, opacity) => ({ 
+    flexDirection: `${direction}`,
+    opacity,
+})
+const nav = (direction, opacity, width, fontSize) => (
+    <div className="nav" style={ navStyle(direction, opacity) }>
+            {navOptions.map( (item, i) => (
+                    <h1 className="menu-item" key={i} style={{ fontSize, width }}>
+                        <Link className="link" to={item.url} >
+                            {item.title}
+                        </Link>
+                    </h1>
+            ))}
+    </div>
+)
 const Nav = ({navOpacity}) => {
-    const navOptions = [
-        { title: 'Products', url: '/products' },
-        { title: 'Info', url: '/info' },
-        { title: 'Contact', url: '/contact' }
-    ]
-    const navStyle = (direction, opacity) => ({ 
-        flexDirection: `${direction}`,
-        opacity,
-    })
-    const nav = (direction = 'column', opacity, fontSize) => (
-        
-        <div className="nav" style={ navStyle(direction, opacity) } >
-                {navOptions.map( (item, i) => (
-                        <h1 className="menu-item" key={i} style={{ fontSize }}>
-                            <Link className="link" to={item.url} >
-                                {item.title}
-                            </Link>
-                        </h1>
-                ))}
-        </div>
-    )
     return (
         <Media query="(max-width: 750px)">
-            { matches => matches ?  nav('column', navOpacity, '1.2em') : nav('row', navOpacity)}
+            { matches => matches ?  nav('column', navOpacity, 'auto', '1.2em') : nav('row', navOpacity, '205px')}
         </Media>
     )}
 
-export default Nav;
+export default Nav
